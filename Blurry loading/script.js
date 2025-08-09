756889c80072b4be6717aaa973b295d99eef55b4
ZoppicoDD <163606755+ZoppicoDD@users.noreply.github.com>
2025-08-09 22:12:50 +0200

    Update script.js

--- Blurry loading/script.js
+++ Blurry loading/script.js
@@ -28,4 +28,5 @@ function LoadContents(){
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+------------------------------------------------------------
 }
